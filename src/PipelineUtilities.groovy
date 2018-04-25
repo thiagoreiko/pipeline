49fce9aac9ee309d53245911038c39bcfc1ca913
Thiago Schiavon <toliveira4@dxc.com>
2018-04-25 15:03:48 -0300

    static method

--- src/PipelineUtilities.groovy
+++ src/PipelineUtilities.groovy
@@ -1,9 +1,15 @@
 package org.foo
-class PipelineUtilities implements Serializable {
+/*class PipelineUtilities implements Serializable {
   def steps
   PipelineUtilities(steps) {this.steps = steps}
   def mvn(args) {
     //steps.sh "${steps.tool 'Maven'}/bin/mvn -o ${args}"
     steps.echo 'dkskdskjjsdsdjjsdkjdskkjdjdsjksdjdkjsda'
   }
+}
+*/
+class PipelineUtilities {
+	static def go(script, args) {
+		script.echo 'testing static method'
+	}
 }
