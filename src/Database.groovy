495055d1f2a720181a1cbd5f32f37c3dda18dd74
Thiago Schiavon <toliveira4@dxc.com>
2018-04-26 15:58:41 -0300

    ffffff

--- src/Database.groovy
+++ src/Database.groovy
@@ -11,6 +11,6 @@ class Database implements Serializable {
   
   def validateScripts() {
     script.echo 'RUNNING VALIDATING SCRIPTS'
-    script.echo ${jsonDb}
+    script.echo "${jsonDb}"
   }
 }
