5921803cea8b8d4d1820b62e264babb1f292fb31
Thiago Schiavon <toliveira4@dxc.com>
2018-04-26 15:30:15 -0300

    creating database class

---
+++ src/Database.groovy
@@ -0,0 +1,16 @@
+package org.foo
+
+class Database implements Serializable {
+  def script
+  def jsonDb
+
+  Database(script, jsonDb) {
+      this.script = script
+      this.jsonDb = jsonDb
+    }
+  
+  def validateScripts() {
+    script.echo 'RUNNING VALIDATING SCRIPTS'
+    script.echo ${script.jsonDb}
+  }
+}
