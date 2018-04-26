8d010726c42003bfc2ab84fc042ef05d95fc143f
Thiago Schiavon <toliveira4@dxc.com>
2018-04-26 10:59:44 -0300

    creating executeSonar method

--- src/PipelineUtilities.groovy
+++ src/PipelineUtilities.groovy
@@ -9,7 +9,16 @@ package org.foo
 }
 */
 class PipelineUtilities {
-	static def go(script, args) {
-		script.echo 'testing static method'
-	}
+	// static def go(script, args) {
+	// 	script.echo 'testing static method'
+	// }
+
+  static def executeSonar(body, sourceCodePath, projectKey, projectName){
+    def sqScannerMsBuildHome = body.tool 'SonarQube Scanner for MSBuild 3.0'
+    body.withSonarQubeEnv {
+      body.bat "\"${sqScannerMsBuildHome}\\MSBuild.SonarQube.Runner.exe\" begin /k:${projectKey} /n:${projectName} /v:1.0"
+      body.bat "\"${body.tool 'MSBuild 15'}\\msbuild.exe\" \"${sourceCodePath}\\.jenkins\\ci.build.task.xml\" /t:CompileSolution /nologo /v:diag /m /nr:false /p:Configuration=Release"
+      body.bat "\"${sqScannerMsBuildHome}\\MSBuild.SonarQube.Runner.exe\" end"
+    }
+  }
 }
