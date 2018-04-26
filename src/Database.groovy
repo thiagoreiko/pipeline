1f7adb7b4862e6386cb7db51c65323b00fe3d468
Thiago Schiavon <toliveira4@dxc.com>
2018-04-26 16:50:44 -0300

    dasdsa

--- src/Database.groovy
+++ src/Database.groovy
@@ -3,16 +3,18 @@ package org.foo
 class Database implements Serializable {
   def script
   def jsonDb
+  def classpath = "D:\\liquibase-bin\\ojdbc6.jar"
+  def driverClassname = "oracle.jdbc.OracleDriver"
 
   Database(script, jsonDb) {
       this.script = script
       this.jsonDb = jsonDb
     }
   
-  def validateScripts() {
-    script.echo 'RUNNING VALIDATING SCRIPTS'
-    // script.echo "${jsonDb}"
+  def validateScripts(credentialsId) {
+    script.echo 'RUNNING VALIDATING SCRIPTS'   
 
-    script.echo script.WORKSPACE
+    script.echo "${classpath}"
+    script.echo "${driverClassname}"
   }
 }
