d9e4bd517c606070a5ece98a2588d9b5c962838f
Thiago Schiavon <toliveira4@dxc.com>
2018-04-27 11:06:28 -0300

    asd

--- src/Database.groovy
+++ src/Database.groovy
@@ -1,19 +1,20 @@
 package org.foo
 
 class Database {
+    @NonCPS
     static def testParallel(script, jsonDb){
         def appliersTest = [:]
 
         (1..5).each {
             appliersTest["${it}"] = {
-                node {
-                    stage("Executando ${it}") {                            
+                script.node {
+                    script.stage("Executando ${it}") {                            
                         script.echo "${it}"
                     }
                 }
             }
         }
-        
+
         script.parallel appliersTest  
     }
 }
