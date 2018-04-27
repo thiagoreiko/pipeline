83185ce1206e72473b9a6dcdd848dd7b8a0817ba
Thiago Schiavon <toliveira4@dxc.com>
2018-04-27 10:48:06 -0300

    changing database class to static

--- src/Database.groovy
+++ src/Database.groovy
@@ -1,83 +1,19 @@
 package org.foo
 
-import groovy.json.JsonSlurper
-import com.cloudbees.groovy.cps.NonCPS
+class Database {
+    static def testParallel(script, jsonDb){
+        def appliersTest = [:]
 
-class Database implements Serializable {
-  def script
-  def jsonDb
-  def scriptsFolderPath
-  def classpath = "D:\\liquibase-bin\\ojdbc6.jar"
-  def driverClassname = "oracle.jdbc.OracleDriver"
-
-  Database(script, jsonDb, scriptsFolderPath = null, classpath = null, driverClassname = null) {
-      this.script = script
-      this.jsonDb = jsonDb
-
-      if(classpath != null) { this.classpath = classpath }
-      
-      if(driverClassname != null) { this.driverClassname = driverClassname }
-
-      if(scriptsFolderPath != null) { this.scriptsFolderPath = scriptsFolderPath }
-      else { this.scriptsFolderPath = "${script.WORKSPACE}\\DB\\" }
-    }
-  
-  def validateScripts(credentialsId) {
-    script.echo 'RUNNING VALIDATING SCRIPTS'   
-    
-    for (db in jsonDb.Databases) {
-        for (sc in db.Schemas) {
-            if(sc.Aplicar) {
-                script.sqlScriptValidator([
-                    changeLogFile : "${scriptsFolderPath}\\${sc.ChangeLogPath}", 
-                    url : "${db.ConnectionString}", 
-                    classpath : "${classpath}", 
-                    driverClassname : "${driverClassname}", 
-                    credentialsId : "${credentialsId}", 
-                    sqlCommands : "drop,truncate", 
-                    validateRollbackScript : false, 
-                    buildFailedWhenInvalid : false
-                ])                
-            }
-        }
-    }
-  }
-
-  def executeScripts(credentialId_update, credentialId_dbDoc){
-      def appliers = [:]
-
-      for (db in jsonDb.Databases) {
-        for (sc in db.Schemas) {
-            if(sc.Aplicar) {
-                appliers["DB_${db.Name}_SCHEMA_${sc.Schema}_${BUILD_NUMBER}"] = {
-                    node {
-                        stage("Executando scripts DB ${db.Name} SCHEMA ${sc.Schema}") {
-                            //executa script
-                             script.liquibaseupdate 
-                                 changelogfile: "${scriptsfolderpath}\\${sc.changelogpath}", 
-                                 classpath: "${classpath}", 
-                                 credentialsid: "${credentialid_update}", 
-                                 driverclassname: "${driverclassname}", 
-                                 tagonsuccessfulbuild: true, 
-                                 testrollbacks: true, 
-                                 url: "${db.connectionstring}"
-                            
-                            //grava dblog
-                             script.liquibasedbdoc 
-                                 changelogfile: "${scriptsfolderpath}\\${sc.changelogpath}", 
-                                 classpath: "${classpath}", 
-                                 credentialsid: "${credentialid_dbdoc}", 
-                                 driverclassname: "${driverclassname}", 
-                                 outputdirectory: ".\\dbdoc\\${db.name}\\${sc.schema}", 
-                                 url: "${db.connectionstring}"
-                        }
+        (1..5).each {
+            appliersTest["${it}"] = {
+                node {
+                    stage("Executando ${it}") {                            
+                        script.echo "${it}"
                     }
                 }
             }
         }
+        
+        script.parallel appliersTest  
     }
-    
-    //executa scripts paralelamente
-    parallel appliers
-  }
 }
