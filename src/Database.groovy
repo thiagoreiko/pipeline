89ec57adf1d3341b70291f418a3b5c56fdd84b2a
Thiago Schiavon <toliveira4@dxc.com>
2018-05-02 15:07:45 -0300

    dsas

--- src/Database.groovy
+++ src/Database.groovy
@@ -50,11 +50,11 @@ class Database implements Serializable {
 
     def test = { val -> script.echo "${val}"}
 
-    def executeScripts(dtBase) {
+    def executeScripts() {
         script.echo "Executing scripts"
         def appliers = [:] 
 
-        def json = new JsonSlurper().parseText(dtBase)
+        def json = new JsonSlurper().parseText()
         for (db in json.Databases) {
             for (sc in db.Schemas) {
                 if(sc.Aplicar) {
