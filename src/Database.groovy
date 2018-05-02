0d83c21725529842daa65b778faa491d683e9d92
Thiago Schiavon <toliveira4@dxc.com>
2018-05-02 11:01:07 -0300

    ab

--- src/Database.groovy
+++ src/Database.groovy
@@ -42,44 +42,6 @@ class Database implements Serializable {
         }
     }
 
-    def executeScripts(){
-      def appliers = [:]      
-      
-      def json = new JsonSlurper().parseText(jsonDb)
-        for (db in json.Databases) {
-            for (sc in db.Schemas) {
-                if(sc.Aplicar) {
-                    appliers["DB_${db.Name}_SCHEMA_${sc.Schema}_${BUILD_NUMBER}"] = {
-                        script.node {
-                            script.stage("Executando scripts DB ${db.Name} SCHEMA ${sc.Schema}") {
-                                //executa script
-                                script.liquibaseUpdate 
-                                    changeLogFile: "${scriptsFolderPath}\\${sc.ChangeLogPath}", 
-                                    classpath: "${classpath}", 
-                                    credentialsId: "${sc.Credenciais}", 
-                                    driverClassname: "${driverClassname}", 
-                                    tagonsuccessfulbuild: true, 
-                                    testrollbacks: true, 
-                                    url: "${db.ConnectionString}"
-                                
-                                //grava dblog
-                                script.liquibaseDbDoc 
-                                    changeLogFile: "${scriptsFolderPath}\\${sc.ChangeLogPath}", 
-                                    classpath: "${classpath}", 
-                                    credentialsId: "${sc.Credenciais}", 
-                                    driverClassname: "${driverClassname}", 
-                                    outputDirectory: ".\\dbdoc\\${db.Name}\\${sc.Schema}", 
-                                    url: "${db.ConnectionString}"
-                            }
-                        }
-                    }
-                }
-            }   
-        }
-    
-      return appliers
-    }
-
     def testVariables() {
         script.echo 'RUNNING VALIDATING SCRIPTS'   
         def json = new JsonSlurper().parseText(jsonDb)
