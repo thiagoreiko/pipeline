4968954faf4b6e980b97b77e19d14808ab19cc14
Thiago Schiavon <toliveira4@dxc.com>
2018-05-02 10:58:20 -0300

    aaaaaaa

--- src/Database.groovy
+++ src/Database.groovy
@@ -21,14 +21,72 @@ class Database implements Serializable {
       else { this.scriptsFolderPath = "${script.WORKSPACE}\\DB\\" }  
     }    
 
-    def testVariables() {
+    def validateScripts() {
         script.echo 'RUNNING VALIDATING SCRIPTS'   
-
         def json = new JsonSlurper().parseText(jsonDb)
         for (db in json.Databases) {
             for (sc in db.Schemas) {
                 if(sc.Aplicar) {
-                    script.echo "${sc.Credenciais}"                  
+                    script.sqlScriptValidator([
+                        changeLogFile : "${scriptsFolderPath}\\${sc.ChangeLogPath}", 
+                        url : "${db.ConnectionString}", 
+                        classpath : "${classpath}", 
+                        driverClassname : "${driverClassname}", 
+                        credentialsId : "${sc.Credenciais}", 
+                        sqlCommands : "drop,truncate", 
+                        validateRollbackScript : false, 
+                        buildFailedWhenInvalid : false
+                    ])                
+                }
+            }
+        }
+    }
+
+    def executeScripts(){
+      def appliers = [:]      
+      
+      def json = new JsonSlurper().parseText(jsonDb)
+        for (db in json.Databases) {
+            for (sc in db.Schemas) {
+                if(sc.Aplicar) {
+                    appliers["DB_${db.Name}_SCHEMA_${sc.Schema}_${BUILD_NUMBER}"] = {
+                        script.node {
+                            script.stage("Executando scripts DB ${db.Name} SCHEMA ${sc.Schema}") {
+                                //executa script
+                                script.liquibaseUpdate 
+                                    changeLogFile: "${scriptsFolderPath}\\${sc.ChangeLogPath}", 
+                                    classpath: "${classpath}", 
+                                    credentialsId: "${sc.Credenciais}", 
+                                    driverClassname: "${driverClassname}", 
+                                    tagonsuccessfulbuild: true, 
+                                    testrollbacks: true, 
+                                    url: "${db.ConnectionString}"
+                                
+                                //grava dblog
+                                script.liquibaseDbDoc 
+                                    changeLogFile: "${scriptsFolderPath}\\${sc.ChangeLogPath}", 
+                                    classpath: "${classpath}", 
+                                    credentialsId: "${sc.Credenciais}", 
+                                    driverClassname: "${driverClassname}", 
+                                    outputDirectory: ".\\dbdoc\\${db.Name}\\${sc.Schema}", 
+                                    url: "${db.ConnectionString}"
+                            }
+                        }
+                    }
+                }
+            }   
+        }
+    
+      return appliers
+    }
+
+    def testVariables() {
+        script.echo 'RUNNING VALIDATING SCRIPTS'   
+        def json = new JsonSlurper().parseText(jsonDb)
+        for (db in json.Databases) {
+            for (sc in db.Schemas) {
+                if(sc.Aplicar) {
+                    script.echo "${sc.Credenciais}"
                 }
             }
         }
