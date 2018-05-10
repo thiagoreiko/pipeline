0eda20f7e3fcd6dcb957f565153852b3bb9b5e7b
Thiago Schiavon <toliveira4@dxc.com>
2018-05-10 17:00:15 -0300

    changing Sonar class

--- src/Sonar.groovy
+++ src/Sonar.groovy
@@ -1,14 +1,18 @@
 package org.foo
 
 class Sonar {
-    
-    static sqScannerMsBuildHome = 'test'
+    def sqScannerMsBuildHome
 
-    static def begin(script) {
-        script.echo "${sqScannerMsBuildHome} - Begin"    
+    Sonar(body) {
+        this.body = body
+        this.sqScannerMsBuildHome = body.tool 'SonarQube Scanner for MSBuild 3.0'
     }
 
-    static def end(script) {
-        script.echo "${sqScannerMsBuildHome} - End"    
+    def begin(key, name) {
+        body.bat "\"${sqScannerMsBuildHome}\\MSBuild.SonarQube.Runner.exe\" begin /k:${key} /n:${name} /v:1.0"
+    }
+
+    def end() {
+        body.bat "\"${sqScannerMsBuildHome}\\MSBuild.SonarQube.Runner.exe\" end"
     }    
 }
