aa7efabf33067db393b6685259d95365253d2ad2
Thiago Schiavon <toliveira4@dxc.com>
2018-05-11 13:58:38 -0300

    122121332321321

--- src/PipelineUtilities.groovy
+++ src/PipelineUtilities.groovy
@@ -65,10 +65,8 @@ class PipelineUtilities {
 
   static def executeApprovalFlow(script, time, submitter, approvalNotificationRecipient) {
     
-    //script.input message: 'Scripts de banco de dados autorizado?', parameters: [choice(choices: 'SIM\nNÃO', description: 'Aprovado?', name: 'APROVADO'), text(defaultValue: '', description: 'Apenas em caso rejeição dos scripts', name: 'JUSTIFICATIVA')], submitter: "${submitter}", submitterParameter: 'APPROVER'
-    script.input message: 'Scripts de banco de dados autorizado?', ok: 'Teste', parameters: [script.choice(choices: 'SIM\nNÃO', description: 'Aprovado', name: 'APROVADO')], submitter: 'ct15184', submitterParameter: 'approved'
-
-
+    script.input message: 'Scripts de banco de dados autorizado?', parameters: [script.choice(choices: 'SIM\nNÃO', description: 'Aprovado?', name: 'APROVADO'), text(defaultValue: '', description: 'Apenas em caso rejeição dos scripts', name: 'JUSTIFICATIVA')], submitter: "${submitter}", submitterParameter: 'APPROVER'
+    
     /*def DBAPPROVAL
 
     script.timeout(time:time, unit:'HOURS') {
