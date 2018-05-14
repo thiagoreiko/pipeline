56460c038f424728f34739fa26def4c8a0a8c691
Thiago Schiavon <toliveira4@dxc.com>
2018-05-14 14:59:08 -0300

    teste

--- src/Database.groovy
+++ src/Database.groovy
@@ -1,5 +1,5 @@
 package org.foo
-import groovy.json.JsonSlurper
+import groovy.json.JsonSlurperClassic
 import org.foo.PipelineUtilities.*
 
 class Database implements Serializable {
@@ -23,16 +23,20 @@ class Database implements Serializable {
     }
 
     @NonCPS
-    def fncValidateScripts(sqlCommands = "drop,truncate", validateRollbackScript = false, buildFailedWhenInvalid = false) {
+    def jsonParse(def json) {
+        new groovy.json.JsonSlurperClassic().parseText(json)
+    }
+    
+    def validateScripts(sqlCommands = "drop,truncate", validateRollbackScript = false, buildFailedWhenInvalid = false) {
         def arr = [:]
-        def json = new JsonSlurper().parseText(jsonDb)
+        def json = jsonParse(jsonDb)
         for (db in json.Databases) {
             for (sc in db.Schemas) {
                 if(sc.Aplicar) {
                     arr["DB_${db.Name}_SCHEMA_${sc.Schema}_${body.BUILD_NUMBER}"] = {
-                        //body.echo "Validating scripts DB "                          
+                        body.echo "Validating scripts DB "                          
 
-                        body.sqlScriptValidator([
+                        /*body.sqlScriptValidator([
                             changeLogFile : "${scriptsFolderPath}\\${sc.ChangeLogPath}", 
                             url : "${db.ConnectionString}", 
                             classpath : "${classpath}", 
@@ -41,7 +45,7 @@ class Database implements Serializable {
                             sqlCommands : sqlCommands, 
                             validateRollbackScript : validateRollbackScript, 
                             buildFailedWhenInvalid : buildFailedWhenInvalid
-                        ])
+                        ])*/
                     }
                 }
             }
@@ -49,120 +53,4 @@ class Database implements Serializable {
 
         return arr
     }
-
-    @NonCPS
-    def fncExecuteScripts() {
-        def arr = [:]
-        def json = new JsonSlurper().parseText(jsonDb)
-        for (db in json.Databases) {
-            for (sc in db.Schemas) {
-                if(sc.Aplicar) {
-                    arr["DB_${db.Name}_SCHEMA_${sc.Schema}_${body.BUILD_NUMBER}"] = {
-                        //body.echo "Executing scripts DB "                          
-
-                        //execute script
-                        body.liquibaseUpdate 
-                            changeLogFile: "${scriptsFolderPath}\\${sc.ChangeLogPath}", 
-                            classpath: "${classpath}", 
-                            credentialsId: "${sc.Credenciais.replace("UUID-", "")}", 
-                            driverClassname: "${driverClassname}", 
-                            tagonsuccessfulbuild: true, 
-                            testrollbacks: true, 
-                            url: "${db.ConnectionString}"
-                        
-                        //save dblog
-                        body.liquibaseDbDoc 
-                            changeLogFile: "${scriptsFolderPath}\\${sc.ChangeLogPath}", 
-                            classpath: "${classpath}", 
-                            credentialsId: "${sc.Credenciais.replace("UUID-", "")}", 
-                            driverClassname: "${driverClassname}", 
-                            outputDirectory: ".\\dbdoc\\${db.Name}\\${sc.Schema}", 
-                            url: "${db.ConnectionString}"
-                    }
-                }
-            }
-        } 
-
-        return arr
-    }    
-
-    @NonCPS
-    def fncPublishReports() {
-        def json = new JsonSlurper().parseText(jsonDb)
-        for (db in json.Databases) {
-            for (sc in db.Schemas) {
-                if(sc.Aplicar) {
-                    body.publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, keepAll: false, 
-										reportDir: "dbDoc\\${db.Name}\\${sc.Schema}", reportFiles: 'index.html', 
-										reportName: 'dbDoc', reportTitles: 'dbDoc'])
-                }
-            }
-        }
-    }
-
-    @NonCPS
-    def fncSaveIdLatestValidScriptLiquibase(environment){
-        def json = new JsonSlurper().parseText(jsonDb)
-        for (db in json.Databases) {
-            for (sc in db.Schemas) {
-                if(sc.Aplicar) {
-                    PipelineUtilities.saveGlobalVars(body, "${db.Name}_SCHEMA_${sc.Schema}_${environment}_LAST_STABLE", "${JOB_NAME}-${BUILD_NUMBER}")
-                }
-            }
-        }
-    }
-
-    @NonCPS
-    def fncRollbackToLatestStableVersion(environment){
-        def fallbacks = [:]
-        def json = new JsonSlurper().parseText(jsonDb)
-        for (db in json.Databases) {
-            for (sc in db.Schemas) {
-                if(sc.Aplicar) {
-                    fallbacks["DB_${db.Name}_SCHEMA_${sc.Schema}_${body.BUILD_NUMBER}"] = {
-                        body.echo "Restoring scripts "
-                        liquibaseRollback 
-                        	changeLogFile: "${scriptsFolderPath}\\${sc.ChangeLogPath}", 
-                        	classpath: "${classpath}", 
-                        	credentialsId: "${sc.Credenciais.replace("UUID-", "")}", 
-                        	driverClassname: "${driverClassname}", 
-                        	rollbackToTag: "${db.Name}_SCHEMA_${sc.Schema}_${environment}_LAST_STABLE",//"${JOB_NAME}-${BUILD_NUMBER}",
-                        	url: "${db.ConnectionString}"
-                    }
-                }
-            }
-        } 
-
-        return fallbacks
-    }
-
-    def validateScripts() {
-        body.echo 'RUNNING VALIDATING SCRIPTS'
-        fncValidateScripts()
-    }
-
-    def executeScripts() {
-        body.echo "Executing scripts"
-        
-        def appliers = [:] 
-        appliers = fncExecuteScripts()
-
-        return appliers       
-    }
-
-    def publishReports(){
-        body.echo 'PUBLISHING REPORTS'
-        fncPublishReports()
-    }
-
-    def saveIdLatestValidScriptLiquibase(environment) {
-        body.echo 'SAVING THE LATEST VALID SCRIPTS'
-        fncSaveIdLatestValidScriptLiquibase(environment)
-    }
-
-    def rollbackToLatestStableVersion(environment) {
-        body.echo 'RESTORING THE LATEST STABLE VERSION OF THE DATABASE'
-        fncRollbackToLatestStableVersion(environment)
-    }
-    
 }
