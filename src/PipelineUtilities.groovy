05ba93f1f0ef97017b3103d704fbb42dfd0b3c65
Thiago Schiavon <toliveira4@dxc.com>
2018-05-15 16:18:48 -0300

    dsasad

--- src/PipelineUtilities.groovy
+++ src/PipelineUtilities.groovy
@@ -44,7 +44,7 @@ class PipelineUtilities {
     }
 
     script.envVars.put(key,value)
-    script.instance.save()
+    instance.save()
   }
 
   static def removeReadOnly(script, workspace = null) {
