adc85c31bf3423917c4e32858971e3114d5a4937
Thiago Schiavon <toliveira4@dxc.com>
2018-05-15 16:52:00 -0300

    aaaaa

--- src/Database.groovy
+++ src/Database.groovy
@@ -1,8 +1,8 @@
 package org.foo
 import groovy.json.JsonSlurperClassic
-import org.foo.PipelineUtilities
+import org.foo.PipelineUtilities.*
 
-class Database implements Serializable {
+class Database extends PipelineUtilities implements Serializable {
 
     def body
     def jsonDb
@@ -104,7 +104,7 @@ class Database implements Serializable {
             for (sc in db.Schemas) {
                 if(sc.Aplicar) {
                     //PipelineUtilities.saveGlobalVars(body, "${db.Name}_SCHEMA_${sc.Schema}_${environment}_LAST_STABLE", "${JOB_NAME}-${BUILD_NUMBER}")
-                    PipelineUtilities.saveGlobalVars(body, "${db.Name}_SCHEMA_${sc.Schema}_${environment}_LAST_STABLE", "${body.JOB_NAME}-${body.BUILD_NUMBER}")
+                    //PipelineUtilities.saveGlobalVars(body, "${db.Name}_SCHEMA_${sc.Schema}_${environment}_LAST_STABLE", "${body.JOB_NAME}-${body.BUILD_NUMBER}")
                 }
             }
         }
