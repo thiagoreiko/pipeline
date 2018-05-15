f70206489b4e2548e813915cb17054f49efd40e2
Thiago Schiavon <toliveira4@dxc.com>
2018-05-15 16:05:07 -0300

    asd

--- src/PipelineUtilities.groovy
+++ src/PipelineUtilities.groovy
@@ -5,7 +5,7 @@ import hudson.model.*
 
 class PipelineUtilities {
 
-  static def saveGlobalVars(script, key, value) {
+  /*static def saveGlobalVars(script, key, value) {
     script.instance = Jenkins.getInstance()
     script.globalNodeProperties = script.instance.getGlobalNodeProperties()
     script.envVarsNodePropertyList = script.globalNodeProperties.getAll(EnvironmentVariablesNodeProperty.class)
@@ -24,7 +24,28 @@ class PipelineUtilities {
     script.envVars.put(key,value)
     script.instance.save()
   }
- 
+  */
+
+  static def saveGlobalVars(script, key, value) {
+    script.instance = Jenkins.getInstance()
+    /*script.globalNodeProperties = script.instance.getGlobalNodeProperties()
+    script.envVarsNodePropertyList = script.globalNodeProperties.getAll(EnvironmentVariablesNodeProperty.class)
+
+    script.newEnvVarsNodeProperty = null
+    script.envVars = null
+
+    if ( script.envVarsNodePropertyList == null || script.envVarsNodePropertyList.size() == 0 ) {
+      script.newEnvVarsNodeProperty = new EnvironmentVariablesNodeProperty();
+      script.globalNodeProperties.add(script.newEnvVarsNodeProperty)
+      script.envVars = script.newEnvVarsNodeProperty.getEnvVars()
+    } else {
+      script.envVars = script.envVarsNodePropertyList.get(0).getEnvVars()
+    }
+
+    script.envVars.put(key,value)
+    script.instance.save()*/
+  }
+
   static def removeReadOnly(script, workspace = null) {
     if (workspace == null){
       workspace = script.WORKSPACE    
