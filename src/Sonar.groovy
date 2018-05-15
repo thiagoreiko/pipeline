7b84e998be2f5a363420348a9e0902d01f481a0f
Thiago Schiavon <toliveira4@dxc.com>
2018-05-15 14:42:00 -0300

    teste

--- src/Sonar.groovy
+++ src/Sonar.groovy
@@ -11,7 +11,7 @@ class Sonar {
 
     def begin(key, name, version, additionalParameter = null) {
         //body.bat "\"${sqScannerMsBuildHome}\\MSBuild.SonarQube.Runner.exe\" begin /k:${key} /n:${name} /v:${version} ${additionalParameter}"
-        body.bat "\"${body.tool 'SonarQube Scanner for MSBuild 3.0'}\\MSBuild.SonarQube.Runner.exe\" begin /k:${key} /n:${name} /v:${version} ${additionalParameter}"
+        body.bat "\"${body.tool 'SonarQube Scanner for MSBuild 3.0'}\\MSBuild.SonarQube.Runner.exe\" begin /k:${key} /n:${name} /v:${version}"
     }
 
     def end() {
