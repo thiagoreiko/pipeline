ace84f6dd165f3340643496c3ee1f28be130ab22
Thiago Schiavon <toliveira4@dxc.com>
2018-05-15 17:13:16 -0300

    dsadsa

--- src/Database.groovy
+++ src/Database.groovy
@@ -106,6 +106,7 @@ class Database implements Serializable {
                 if(sc.Aplicar) {
                     //PipelineUtilities.saveGlobalVars(body, "${db.Name}_SCHEMA_${sc.Schema}_${environment}_LAST_STABLE", "${JOB_NAME}-${BUILD_NUMBER}")
                     //PipelineUtilities.saveGlobalVars(body, "${db.Name}_SCHEMA_${sc.Schema}_${environment}_LAST_STABLE", "${body.JOB_NAME}-${body.BUILD_NUMBER}")
+                    body.echo "REGISTRANDO ${db.Name}_SCHEMA_${sc.Schema}_${environment}_LAST_STABLE"
                     pipe.saveGlobalVars(body, "${db.Name}_SCHEMA_${sc.Schema}_${environment}_LAST_STABLE", "${body.JOB_NAME}-${body.BUILD_NUMBER}")
                 }
             }
