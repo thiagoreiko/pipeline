00cf97d2250cb9fd572548b7931a6df3afb701e6
Thiago Schiavon <toliveira4@dxc.com>
2018-05-15 14:54:47 -0300

    ajustes no Sonar

--- src/Sonar.groovy
+++ src/Sonar.groovy
@@ -1,24 +1,17 @@
 package org.foo
 
-class Sonar {
-    def sqScannerMsBuildHome
+class Sonar {    
     def body
     
     Sonar(body) {
-        this.body = body
-        //this.sqScannerMsBuildHome = body.tool 'SonarQube Scanner for MSBuild 3.0'
+        this.body = body     
     }
 
-    def begin(key, name, version, additionalParameter = null) {
-        def addParam = ""
-        if(additionalParameter != null){addparam = additionalParameter}
-        //body.bat "\"${sqScannerMsBuildHome}\\MSBuild.SonarQube.Runner.exe\" begin /k:${key} /n:${name} /v:${version} ${additionalParameter}"
-        body.bat "\"${body.tool 'SonarQube Scanner for MSBuild 3.0'}\\MSBuild.SonarQube.Runner.exe\" begin /k:${key} /n:${name} /v:${version} ${addParam}"
+    def begin(key, name, version, additionalParameter = "") {
+        body.bat "\"${body.tool 'SonarQube Scanner for MSBuild 3.0'}\\MSBuild.SonarQube.Runner.exe\" begin /k:${key} /n:${name} /v:${version} ${additionalParameter}"
     }
 
-    def end() {
-        //body.bat "\"${sqScannerMsBuildHome}\\MSBuild.SonarQube.Runner.exe\" end"
+    def end() {        
         body.bat "\"${body.tool 'SonarQube Scanner for MSBuild 3.0'}\\MSBuild.SonarQube.Runner.exe\" end"
     }
-
 }
