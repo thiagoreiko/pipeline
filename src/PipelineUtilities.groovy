7e8888b460cf1ce11cbd73a8e160168459826fdc
Thiago Schiavon <toliveira4@dxc.com>
2018-05-15 16:20:25 -0300

    Finalizado o método de salvar valores nas variáveis globais do jenkins

--- src/PipelineUtilities.groovy
+++ src/PipelineUtilities.groovy
@@ -5,30 +5,8 @@ import hudson.model.*
 
 class PipelineUtilities {
 
-  /*static def saveGlobalVars(script, key, value) {
-    script.instance = Jenkins.getInstance()
-    script.globalNodeProperties = script.instance.getGlobalNodeProperties()
-    script.envVarsNodePropertyList = script.globalNodeProperties.getAll(EnvironmentVariablesNodeProperty.class)
-
-    script.newEnvVarsNodeProperty = null
-    script.envVars = null
-
-    if ( script.envVarsNodePropertyList == null || script.envVarsNodePropertyList.size() == 0 ) {
-      script.newEnvVarsNodeProperty = new EnvironmentVariablesNodeProperty();
-      script.globalNodeProperties.add(script.newEnvVarsNodeProperty)
-      script.envVars = script.newEnvVarsNodeProperty.getEnvVars()
-    } else {
-      script.envVars = script.envVarsNodePropertyList.get(0).getEnvVars()
-    }
-
-    script.envVars.put(key,value)
-    script.instance.save()
-  }
-  */
-
   static def saveGlobalVars(script, key, value) {
     def instance = Jenkins.getInstance()
-    //script.instance = Jenkins.getInstance()
     def globalNodeProperties = instance.getGlobalNodeProperties()
     def envVarsNodePropertyList = globalNodeProperties.getAll(EnvironmentVariablesNodeProperty.class)
 
