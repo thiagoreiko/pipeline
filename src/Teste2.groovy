19050f9a47eacff125819bdc6d9be386b132f954
Thiago Schiavon <toliveira4@dxc.com>
2018-05-16 09:06:42 -0300

    dsa

--- src/Teste2.groovy
+++ src/Teste2.groovy
@@ -3,6 +3,6 @@ package org.foo
 
 static def testando() {
     def sh = new GroovyShell()  
-    File f = new File("Teste1.groovy");  
+    File f = new File("../Teste1.groovy");  
     def closure = sh.evaluate(f.text);
 }
