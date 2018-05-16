61abd35132cc4b85cd00fcfd6f032ff145b7c4b2
Thiago Schiavon <toliveira4@dxc.com>
2018-05-16 09:14:55 -0300

    dsadsds

--- src/Teste2.groovy
+++ src/Teste2.groovy
@@ -3,7 +3,8 @@ package org.foo
 import DbUtils.*
 
 def testando() {
-    def dbUtils = new DbUtils()
-    def something = 'foobar'
-    dbUtils.save(something)
+    // def dbUtils = new DbUtils()
+    // def something = 'foobar'
+    // dbUtils.save(something)
+    evaluate(new File("../src/Teste1.groovy"))
 }
